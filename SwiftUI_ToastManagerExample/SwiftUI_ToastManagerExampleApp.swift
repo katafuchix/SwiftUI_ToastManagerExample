3ae48d83de5f961785186acbd8515186ece11a09
katafuchix <katafuchix@gmail.com>
2026-06-04 11:22:59 +0900

    Initial Commit

---
+++ SwiftUI_ToastManagerExample/SwiftUI_ToastManagerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ToastManagerExampleApp.swift
+//  SwiftUI_ToastManagerExample
+//
+//  Created by cano on 2026/06/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ToastManagerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
